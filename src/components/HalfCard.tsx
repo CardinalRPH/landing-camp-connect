e6b8a9394208c5fd9e9e9a3ad9ed83cf220e1f82
CardinalRPH <rayhanfebriyan62@gmail.com>
2025-06-20 20:38:33 +0700

    update card

--- src/components/HalfCard.tsx
+++ src/components/HalfCard.tsx
@@ -9,7 +9,7 @@ const HalfCard = ({imageUrl, altText, children}:HalfCardProps)=> {
     return (
         <div className="relative w-full lg:w-1/2 h-fit">
             <img src={imageUrl} alt={altText} className="aspect-[3/4] object-cover w-full h-9/12 rounded-2xl" />
-            <div className="absolute top-10 left-10">
+            <div className="absolute top-5 lg:top-10 left-5 lg:left-10">
                 {children}
             </div>
         </div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -20,7 +20,7 @@ const Home = () => {
             <h5 className="text-white text-lg">
               Explore the 50+ campsite we're launching as part of our places
             </h5>
-            <button className="bg-white text-black p-2 rounded-xl cursor-pointer text-nowrap">
+            <button className="bg-white text-black py-1 px-2 rounded-xl cursor-pointer text-nowrap">
               Learn more
             </button>
           </div>
@@ -31,7 +31,7 @@ const Home = () => {
               className="w-full rounded-2xl aspect-video"
             />
             <div className="absolute flex bottom-10  w-full  items-center flex-col">
-              <h1 className="text-white text-4xl text-center font-bold py-2">
+              <h1 className="text-white text-2xl lg:text-4xl text-center font-bold py-2">
                 Not sure where to go? Perfect.
               </h1>
               <button
@@ -74,10 +74,10 @@ const Home = () => {
             />
             <div className="absolute bottom-0 w-full h-full flex">
               <div className="w-full lg:w-1/2 flex flex-col justify-center text-center">
-                <h1 className="text-white text-2xl font-medium my-2">
+                <h1 className="text-white text-xl lg:text-2xl font-medium lg:my-2">
                   Introduce
                 </h1>
-                <h1 className="text-6xl text-white font-bold my-2">Camp Card</h1>
+                <h1 className="text-4xl lg:text-6xl text-white font-bold lg:my-2">Camp Card</h1>
                 <p className="text-white my-2">Get all access to our campsite</p>
                 <div className="">
                   <button
@@ -93,7 +93,7 @@ const Home = () => {
           <MainCard title="Discover the Activities" id="activities">
             <div className="flex gap-5">
               <HalfCard imageUrl="https://i.pinimg.com/564x/aa/32/e8/aa32e8e5d66779890f564c51e9ec31a3.jpg">
-                <h1 className="text-white text-3xl lg:text-5xl font-medium">
+                <h1 className="text-white text-2xl lg:text-6xl font-medium">
                   Things to do on your camp
                 </h1>
                 <button
@@ -104,7 +104,7 @@ const Home = () => {
                 </button>
               </HalfCard>
               <HalfCard imageUrl="https://theexpertcamper.co.uk/wp-content/uploads/2024/06/how-to-make-your-camping-trip-more-comfortable-ZF.jpeg">
-                <h1 className="text-white text-3xl lg:text-5xl font-medium">
+                <h1 className="text-white text-2xl lg:text-6xl font-medium">
                   Things to bring on your camp
                 </h1>
                 <button
@@ -123,9 +123,9 @@ const Home = () => {
               className="w-full rounded-2xl object-cover aspect-video"
             />
             <div className="absolute top-0 w-full h-full flex">
-              <div className="w-1/2 flex flex-col justify-around m-10">
-                <h1 className="text-white text-3xl lg:text-4xl font-medium">Question About Your Camp?</h1>
-                <div className="">
+              <div className="w-1/2 flex flex-col justify-around m-2 lg:m-10">
+                <h1 className="text-white text-2xl lg:text-4xl font-medium">Question About Your Camp?</h1>
+                <div className="my-2">
                   <button
                     type="button"
                     className="py-2 px-5 rounded-xl bg-white cursor-pointer"
